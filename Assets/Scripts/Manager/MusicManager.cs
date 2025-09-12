e130cac51b6ac423c0a9f76f25caf8e96e5fc48b
KiraWTNB8201 <mcd2476076@stu.o-hara.ac.jp>
2025-09-12 15:53:55 +0900

    1553

--- Assets/Scripts/Manager/MusicManager.cs
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -2,36 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour{
+public class MusicManager : MonoBehaviour {
 
     public static MusicManager instance = null;
     [SerializeField] SongDataBase dataBase;
-    new AudioSource audio;
-    AudioClip Music;
-    public string songName;
-    bool played;
-    float time = 0;
+    private AudioSource audio;
+    private AudioClip music;
 
-    void Start(){
+    public string songName;
+    private bool played;
+
+    // dspTimeで管理するための基準
+    private double songStartDspTime;
+
+    void Start() {
         instance = this;
         GameManager.instance.start = false;
-        songName = dataBase.songData[GameManager.instance.songID].songName;   //読み込むファイル名
-        audio = GetComponent<AudioSource>();    //オーディオファイルを入れる
-        Music = (AudioClip)Resources.Load("Musics/" + songName);
+
+        songName = dataBase.songData[GameManager.instance.songID].songName;
+        audio = GetComponent<AudioSource>();
+        music = Resources.Load<AudioClip>("Musics/" + songName);
+
         played = false;
     }
 
     void Update() {
+        // スタート操作があったら曲を予約して開始
         if (Input.anyKeyDown && !played) {
             GameManager.instance.start = true;
-            GameManager.instance.startTime = Time.time;
+
+            // 現在のdspTimeを基準に曲の再生を予約
+            double startTime = AudioSettings.dspTime + (GameManager.instance.settingData.songOffset / 1000.0);
+            audio.clip = music;
+            audio.volume = GameManager.instance.settingData.BGMVolume / 100f;
+            audio.PlayScheduled(startTime);
+
+            // dsp基準で開始時刻を保存
+            songStartDspTime = startTime;
+            played = true;
         }
-        if (GameManager.instance.start) {
-            time += Time.deltaTime;
-            if((time >= GameManager.instance.settingData.songOffset / 1000) && !played) {
-                audio.PlayOneShot(Music, GameManager.instance.settingData.BGMVolume / 100);
-                played = true;
-            }            
-        }
+    }
+
+    // 曲の経過時間をdsp基準で返す
+    public double GetSongTime() {
+        return AudioSettings.dspTime - songStartDspTime;
     }
 }
